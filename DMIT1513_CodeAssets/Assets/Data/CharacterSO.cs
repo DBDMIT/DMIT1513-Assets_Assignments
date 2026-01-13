72fb4ad9d3da5f549db00ca92d3004a759b82f36
DBDMIT <dbeaver10@nait.ca>
2026-01-13 12:06:38 -0700

    Whatever

---
+++ DMIT1513_CodeAssets/Assets/Data/CharacterSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterSO", menuName = "Character/CharacterSO")]
+public class CharacterSO : ScriptableObject
+{
+    public string characterName;
+    public Sprite characterSprite;
+
+
+}
