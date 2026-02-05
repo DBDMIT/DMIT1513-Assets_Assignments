b453836afbb798db13160c0b58c073e28a6733e2
DBDMIT <dbeaver10@nait.ca>
2026-02-05 14:28:14 -0700

    update

--- DMIT1513_Assets/Assets/Scenes/Lesson10_Feb05/Scripts/MovingGameObject.cs
+++ DMIT1513_Assets/Assets/Scenes/Lesson10_Feb05/Scripts/MovingGameObject.cs
@@ -6,25 +6,28 @@ public class MovingGameObject : MonoBehaviour
     public Vector3 startingPosition;
     public float moveSpeed;
     private Vector3 moveDirection = Vector3.right;
+    public Rigidbody rb;
 
     public void Start()
     {
         startingPosition = transform.position;
         moveDirection = maxMoveRange.normalized;
+        rb = GetComponent<Rigidbody>();
     }
 
-    public void Update()
+    public void FixedUpdate()
     {
         Vector3 nextPosition = transform.position + moveDirection * moveSpeed * Time.deltaTime;
-        Vector3 offset = nextPosition - transform.position;
+        Vector3 offset = nextPosition - startingPosition;
 
         if (Mathf.Abs(offset.x) > maxMoveRange.x || // .Abs gives you the scale of the number, not the value (-10 becomes just 10).
-            Mathf.Abs(offset.x) > maxMoveRange.y ||
-            Mathf.Abs(offset.x) > maxMoveRange.z) 
+            Mathf.Abs(offset.y) > maxMoveRange.y ||
+            Mathf.Abs(offset.z) > maxMoveRange.z) 
         {
             moveDirection *= -1;
+            nextPosition = transform.position + moveDirection * moveSpeed * Time.deltaTime;
         }
 
-        transform.position = nextPosition;
+        rb.MovePosition(nextPosition);
     }
 }
