2b63f6536f1911fcc0a542676539ec3f290130c8
DBDMIT <dbeaver10@nait.ca>
2026-04-10 05:52:18 -0600

    Screen stuff

--- Ass4/Assets/Scripts/screen_interaction.cs
+++ Ass4/Assets/Scripts/screen_interaction.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -6,6 +7,7 @@ using UnityEngine.UI;
 public class screen_interaction : MonoBehaviour
 {
     public InputAction screenInput;
+    bool isActive = false;
 
     private void Start()
     {
@@ -15,6 +17,14 @@ public class screen_interaction : MonoBehaviour
 
     public void SwitchCamera(InputAction.CallbackContext c)
     {
-        
+        if (isActive)
+        {
+            Debug.Log("Screen should switch now");
+        }
+    }
+
+    public void ToggleScreenInput()
+    {
+        isActive = !isActive;
     }
 }
---
+++ Ass4/Assets/Scripts/screen_switch.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screen_switch : MonoBehaviour
+{  
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
