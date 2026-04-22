30efa35cbfdb6ed58aeb7ec6d0ccbf3fbdb22af5
DBDMIT <dbeaver10@nait.ca>
2026-04-21 21:39:28 -0600

    Menu Redo
    Needs selection logic

---
+++ Ass5/Assets/Scripts/menu_parallax.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class menu_parallax : MonoBehaviour
+{
+    public float offsetMultiplier = 1f;
+    public float smoothTime = 0.3f;
+
+    private Vector2 startPosition;
+    private Vector3 velocity;
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        Vector2 offset = Camera.main.ScreenToViewportPoint(Mouse.current.position.ReadValue());
+        transform.position = Vector3.SmoothDamp(transform.position, startPosition + (offset * offsetMultiplier), ref velocity, smoothTime);
+    }
+}
